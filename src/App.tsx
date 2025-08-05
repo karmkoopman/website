69f44fa120358ed110250ec8512c0db0ab731766
karmkoopman <karmkoopman@gmail.com>
2025-08-05 11:54:33 +0200

    Fix GitHub Pages deployment - add base path configuration

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/website">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/over-ons" element={<OverOns />} />
