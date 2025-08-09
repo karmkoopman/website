fa661024b11576a69f0b64f908aa0d34eca38d47
Sander Roesink <sander@roesink.dev>
2025-08-09 08:43:27 +0200

    Fix base path

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/website">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/over-ons" element={<OverOns />} />
